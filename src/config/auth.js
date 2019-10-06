045314336765a899a5d332344a12cb3a3cb0f719
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-06 11:35:57 -0300

    feature: jwt authentication

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  // meetapprocketseatfinalchallenge
+  secret: '805d7485540d0a7c87cf174fb4cca3ec',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 // User routes
 routes.post('/users', UserController.store);
+
+// Sessions routes
+routes.post('/sessions', SessionController.store);
 
 export default routes;
