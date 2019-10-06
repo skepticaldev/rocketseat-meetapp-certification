188bfac0aca678b527209dff1b211c36e5bccc83
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-06 11:00:27 -0300

    feature: generate password hash

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -7,12 +8,24 @@ class User extends Model {
         // all columns that user could insert, receive or update
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
       }
     );
+
+    // Executes before any user save
+    this.addHook('beforeSave', async user => {
+      // Checks if user is signup ou updating a user
+      if (user.password) {
+        // Encrypt user password
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
