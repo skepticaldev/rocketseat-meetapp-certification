39bb6717963a33393064922cdf973e7bd2616e2f
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-06 10:49:34 -0300

    feature: store users

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-// Test database connection
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Philipe Cavalcanti',
-    email: 'philipe@test.com.br',
-    password_hash: '12345678 ',
-  });
-
-  return res.json(user);
-});
+// User routes
+routes.post('/users', UserController.store);
 
 export default routes;
