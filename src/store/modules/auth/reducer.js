e525f8f9c1c66a7c4d80fd3cff841dd9bb45075e
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-13 16:17:01 -0300

    chore: authentication

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,10 +1,19 @@
+import produce from 'immer';
+
 const INITIAL_STATE = {
-  token: 1234,
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS':
+        draft.token = action.payload.token;
+        draft.signed = true;
+        break;
+      default:
+    }
+  });
 }
