7205a3bf21786ee056e88f447559745a019d7664
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-13 13:53:58 -0300

    chore: config root import

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUp';
-import Dashboard from '../pages/Dashboard';
-import Profile from '../pages/Profile';
-import MeetupManage from '../pages/MeetupManage';
+import SignIn from '~/pages/SignIn';
+import SignUp from '~/pages/SignUp';
+import Dashboard from '~/pages/Dashboard';
+import Profile from '~/pages/Profile';
+import MeetupManage from '~/pages/MeetupManage';
 
 export default function Routes() {
   return (
