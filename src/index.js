308376aa28908378c9f4e1001f189c4bafeb50cc
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-19 15:55:16 -0300

    config: reactotron config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
-// import { Container } from './styles';
 
 export default function App() {
   return (
