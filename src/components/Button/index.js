85fe5871b9b9545930eabaae1347dae0fd750a30
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-19 12:44:58 -0300

    chore: create input and button

---
+++ src/components/Button/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ActivityIndicador } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { Container, Text } from './styles';
+
+export default function Button({ children, loading, ...rest }) {
+  return (
+    <Container {...rest}>
+      {loading ? (
+        <ActivityIndicador size="small" color="#FFF" />
+      ) : (
+        <Text>{children}</Text>
+      )}
+    </Container>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  loading: false,
+};
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Backgorund from '~/components/Background';
 // import { Container } from './styles';
 
@@ -8,6 +10,13 @@ export default function SignIn() {
   return (
     <Backgorund>
       <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Backgorund>
   );
 }
