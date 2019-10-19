c08130a33d57fe097cea72aea219590a0349d8c6
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-19 12:05:36 -0300

    chore: authentication routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+import Routes from './routes';
 // import { Container } from './styles';
 
 export default function App() {
-  return <View />;
+  return <Routes />;
 }
