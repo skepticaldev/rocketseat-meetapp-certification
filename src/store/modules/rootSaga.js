3100cde437be58a3ccb680bfc3b35bcc1bad657f
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-26 18:19:57 -0300

    chore: remove schedule module

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -2,10 +2,9 @@ import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
 import user from './user/sagas';
-import schedule from './schedule/sagas';
 import subscriptions from './subscriptions/sagas';
 import meetups from './meetups/sagas';
 
 export default function* rootSaga() {
-  return yield all([auth, user, schedule, subscriptions, meetups]);
+  return yield all([auth, user, subscriptions, meetups]);
 }
