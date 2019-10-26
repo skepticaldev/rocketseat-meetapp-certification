d8566fd6bffb20167602d95e817139448da0da8c
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-26 20:42:47 -0300

    fix: change reactotron variable name

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,10 +2,10 @@ import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import reactotronSaga from 'reactotron-redux-saga';
 
-import { physicalDevice } from '~/util/constants/address';
+import { myAddress } from '~/util/constants/address';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: physicalDevice })
+  const tron = Reactotron.configure({ host: myAddress })
     .useReactNative()
     .use(reactotronRedux())
     .use(reactotronSaga())
