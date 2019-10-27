1a43cb2b28d1a9fc572203118ece7105814c4e35
skepticaldev <philipe.cavalcanti.lp@gmail.com>
2019-10-27 13:33:06 -0300

    chore: redirect user to login screen after signup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import createRouter from './routes';
 
+import * as NavigationService from '~/services/navigation';
+
 export default function App() {
   const signed = useSelector(state => state.auth.signed);
 
   const Routes = createRouter(signed);
 
-  return <Routes />;
+  return <Routes ref={nav => NavigationService.setNavigator(nav)} />;
 }
